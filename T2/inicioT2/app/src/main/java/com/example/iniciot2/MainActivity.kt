dce84512a7220f464ee9c046d30c51c59b2f1550
DavidVinagrero <david.i.vinagrero.e@gmail.com>
2022-10-14 16:38:15 +0200

    update

--- T2/inicioT2/app/src/main/java/com/example/iniciot2/MainActivity.kt
+++ T2/inicioT2/app/src/main/java/com/example/iniciot2/MainActivity.kt
@@ -2,28 +2,68 @@ package com.example.iniciot2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var botonInicial: Button
     lateinit var etiquetaSaludo: TextView
+    lateinit var editNombre: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.v("ciclo_vida","Ejecutando metodo onCreate")
         // Asocia la parte lógica y la gráfica
         setContentView(R.layout.activity_main)
+
         instancias()
         acciones()
     }
 
     private fun acciones() {
-        botonInicial.setOnClickListener({etiquetaSaludo.setText("Hola, primera app completa")})
+        botonInicial.setOnClickListener{
+            if(!editNombre.text.isEmpty()){
+                var notification= Snackbar.make(it,"¿Estás seguro, "+editNombre.text+"?",Snackbar.LENGTH_INDEFINITE)
+                notification.setAction("Aceptar",{
+                    etiquetaSaludo.setText(editNombre.text)
+                    editNombre.setText("")
+                })
+                notification.show()
+            } else{
+                Log.v("edit_nombre","Texto vacío")
+                Toast.makeText(applicationContext,"Texto vacío",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun instancias(){
         this.botonInicial= findViewById(R.id.boton_inicial)
         this.etiquetaSaludo= findViewById(R.id.texto_saludo)
+        this.editNombre= findViewById(R.id.edit_nombre)
+    }
+
+    override fun onStart() {
+        Log.v("ciclo_vida","Ejecutando metodo onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.v("ciclo_vida","Ejecutando metodo onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.v("ciclo_vida","Ejecutando metodo onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.v("ciclo_vida","Ejecutando metodo onStop")
+        super.onStop()
     }
 }
